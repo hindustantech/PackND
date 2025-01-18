7a52c107e2ad6e18f75de8dd690cb09bab666c7a
umesh <umesh98904@gmail.com>
2025-01-18 13:28:57 +0530

    Removed .env from version control

--- src/index.js
+++ src/index.js
@@ -2,21 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { DataContext } from './DataContext';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+
+// Verify if the Client ID is loaded
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <GoogleOAuthProvider clientId='1049011506017-8b31tv27vbi4suls3pbb4j3ijfuaeqsm.apps.googleusercontent.com'>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
     <App />
   </GoogleOAuthProvider>
-
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
